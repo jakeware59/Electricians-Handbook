53f6ce83bb11b5303ba827c26a8e210a6bfb2d06
jakeware59 <jakeware59@gmail.com>
2026-07-17 23:05:18 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ratchets-handbook-v0.2';
+const ASSETS=['./','./index.html','./app.css?v=2','./app.js?v=2','./manifest.json?v=2','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
